fadf5cd205fe56c54513cc1b536f19dddd306654
Lucas Silva <lucas.roberto@creditas.com>
2024-06-30 22:32:26 -0300

    fix root route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Twilio\Rest\Client;
 
-Route::get('/',[\App\Http\Controllers\ConversationalController::class, 'sendDashboardReport']);
-
 Route::post('/new_message',[\App\Http\Controllers\ConversationalController::class, 'new_message']);
 Route::post('/status',[\App\Http\Controllers\ConversationalController::class, 'status']);
